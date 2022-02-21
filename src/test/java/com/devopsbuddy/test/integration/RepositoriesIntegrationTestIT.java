5fdf6c1fe9776803dff6a2b9250bdaea8ddcb53e
Ubuntu <ubuntu@ip-172-31-28-141.us-east-2.compute.internal>
2022-02-21 23:54:58 +0000

    setup h2 console and repository layer. junit5 integration test

---
+++ src/test/java/com/devopsbuddy/test/integration/RepositoriesIntegrationTestIT.java
@@ -0,0 +1,123 @@
+package com.devopsbuddy.test.integration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import com.devopsbuddy.DevopsbuddyApplication;
+import com.devopsbuddy.backend.persistence.domain.backend.Plan;
+import com.devopsbuddy.backend.persistence.domain.backend.Role;
+import com.devopsbuddy.backend.persistence.domain.backend.User;
+import com.devopsbuddy.backend.persistence.domain.backend.UserRole;
+import com.devopsbuddy.backend.persistence.repositories.PlanRepository;
+import com.devopsbuddy.backend.persistence.repositories.RoleRepository;
+import com.devopsbuddy.backend.persistence.repositories.UserRepository;
+import com.devopsbuddy.enums.PlansEnum;
+import com.devopsbuddy.enums.RolesEnum;
+import com.devopsbuddy.utils.UsersUtils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Sort;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = DevopsbuddyApplication.class)
+
+// @WebAppConfiguration
+// @DataJpaTest
+public class RepositoriesIntegrationTestIT {
+    
+    @Autowired
+    private PlanRepository planRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    public void init() {
+
+        assertNotNull(planRepository);
+        assertNotNull(roleRepository);
+        assertNotNull(userRepository);
+    }
+
+    
+    @Test
+    public void testCreateNewPlan() throws Exception {
+        Plan basicPlan = createPlan(PlansEnum.BASIC);
+        planRepository.save(basicPlan);
+        Plan retrievedPlan = planRepository.findById(PlansEnum.BASIC.getId()).orElse(null);
+        assertNotNull(retrievedPlan);
+    }
+
+    @Test
+    public void testCreateNewRole() throws Exception {
+
+        Role userRole  = createRole(RolesEnum.BASIC);
+        roleRepository.save(userRole);
+
+        Role retrievedRole = roleRepository.findById(RolesEnum.BASIC.getId()).orElse(null);
+        assertNotNull(retrievedRole);
+    }
+
+    @Test
+    public void createNewUser() throws Exception {
+
+        Plan basicPlan = createPlan(PlansEnum.BASIC);
+        planRepository.save(basicPlan);
+
+        User basicUser = UsersUtils.createBasicUser();
+        basicUser.setPlan(basicPlan);
+
+        Role basicRole = createRole(RolesEnum.BASIC);
+        Set<UserRole> userRoles = new HashSet<>();
+        UserRole userRole = new UserRole(basicUser, basicRole);
+        userRoles.add(userRole);
+
+        basicUser.getUserRoles().addAll(userRoles);
+
+        for (UserRole ur : userRoles) {
+            roleRepository.save(ur.getRole());
+        }
+
+        basicUser = userRepository.save(basicUser);
+        User newlyCreatedUser = userRepository.findById(basicUser.getId()).orElse(null);
+        assertNotNull(newlyCreatedUser);
+        assertTrue(newlyCreatedUser.getId() != 0);
+        assertNotNull(newlyCreatedUser.getPlan());
+        assertNotNull(newlyCreatedUser.getPlan().getId());
+        Set<UserRole> newlyCreatedUserUserRoles = newlyCreatedUser.getUserRoles();
+        for (UserRole ur : newlyCreatedUserUserRoles) {
+            assertNotNull(ur.getRole());
+            assertNotNull(ur.getRole().getId());
+        }
+
+    }
+
+    //-----------------> Private methods
+
+    private Plan createPlan(PlansEnum plansEnum) {
+        return new Plan(plansEnum);
+    }
+
+    private Role createRole(RolesEnum rolesEnum) {
+        return new Role(rolesEnum);
+    }
+
+}
